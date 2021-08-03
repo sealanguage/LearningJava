27e0a1715182698ac4b981ed8f6becb42392632d
lane froehlich <elaine.froehlich@gmail.com>
2021-08-02 22:37:50 -0400

    added conditional statements to validate bidding values

--- liarsDice/src/com/company/Die.java
+++ liarsDice/src/com/company/Die.java
@@ -23,4 +23,8 @@ public class Die {
         return faceUpValue;
     }
 
+    // public void removeDie() {
+        //  remove a die
+    //  }
+
 }
--- liarsDice/src/com/company/Main.java
+++ liarsDice/src/com/company/Main.java
@@ -1,57 +1,60 @@
 package com.company;
 
+import java.net.StandardSocketOptions;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        String name;
         Cup cup = new Cup();
         Scanner scanner = new Scanner(System.in);
         System.out.println("What is your name?");
         String playerName = scanner.nextLine();
 //        System.out.printf(playerName);
 
-
 //        public Player(String name) {
 //            this.name = name;
-//
 //        }
 
         Cup myCup = new Cup();
         myCup.roll();
         System.out.println(playerName + "'s roll is:  " + myCup.displayCup());
 
-        String bidNumberDice;
-        int bidFaceUpValue;
-        Scanner scanner1 = new Scanner((System.in));
-        System.out.println("Make a bid: numberOfDice: ");
-        bidNumberDice = scanner.nextLine();
-//        System.out.println(bidNumberDice);
+        int diceQuantity = 0;
+        int faceUpValue = 0;
+        int playerDiceQuantityBid = 0;
+        int playerFaceUpValueBid = 0;
+
+        System.out.println("Make a bid: number of dice: ");
+        playerDiceQuantityBid = scanner.nextInt();
+        //todo: make number of dice equal the number of dice in player's cup
+        if (playerDiceQuantityBid < 1 || playerDiceQuantityBid > 6) {
+            System.out.println("Invalid");
+        }
 
         System.out.println("Make a bid face up value of dice: ");
-        bidFaceUpValue = scanner.nextInt();
-//        System.out.println(bidFaceUpValue);
+        playerFaceUpValueBid = scanner.nextInt();
+        if (playerFaceUpValueBid < 1 || playerFaceUpValueBid > 6) {
+            System.out.println("Invalid");
+        }
 
-        System.out.println(playerName + "'s bid is: " + bidNumberDice + " / " + bidFaceUpValue + "'s");
+        System.out.println(playerName + "'s bid is: " + playerDiceQuantityBid + " / " + playerFaceUpValueBid + "'s");
 
-        //        Scanner scanner = new Scanner(System.in);
-//        System.out.println("What is your first name?");
-//        String input = scanner.nextLine();
-//        System.out.println("What is your last name?");
-//        String last = scanner.nextLine();
-        //
-//        System.out.println("Hello " + last + ", " + input + " " + last);
+        if (playerFaceUpValueBid > faceUpValue && playerDiceQuantityBid > diceQuantity) {
+            System.out.println("Bid is valid");
+            faceUpValue = playerFaceUpValueBid;
+            diceQuantity = playerDiceQuantityBid;
+            System.out.println(faceUpValue + " " + diceQuantity +"'s");
+        }
+        else {
+            System.out.println("Bid is not valid, bid: number of dice: ");
+            playerDiceQuantityBid = scanner.nextInt();
+            System.out.println("Bid is not valid, bid: face up value: ");
+            playerFaceUpValueBid = scanner.nextInt();
+        }
 
 
-        // create bidNumberOfDice and bidFaceUpValue variables
-        // currentBid is numberOfDice / faceUpValue
-        // if bidNumberOfDice > numberOfDice or bidFaceUpValue > faceUpValue
-        // bid is valid and can be accepted as currentBid
-        // pass bidNumberOfDice to numberOfDice and bidFaceUpValue to faceUpValue
-        // else
-        // bid is not vaild, ask player to bid again
 
 
 
