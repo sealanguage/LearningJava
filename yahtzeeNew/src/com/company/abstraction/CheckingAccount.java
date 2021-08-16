5e716f138060ba82d3fbee350363b4b1a0e33fe3
lane froehlich <elaine.froehlich@gmail.com>
2021-08-16 19:49:13 -0400

    adding

--- yahtzeeNew/src/com/company/abstraction/CheckingAccount.java
+++ yahtzeeNew/src/com/company/abstraction/CheckingAccount.java
@@ -5,4 +5,11 @@ public class CheckingAccount {
     private long balance;
     private String name;
     private String pin;
+
+    public CheckingAccount (String id, long balance, String name, String pin) {
+        this.id = id;
+        this.balance = balance;
+        this.name = name;
+        this.pin = pin;
+    }
 }
